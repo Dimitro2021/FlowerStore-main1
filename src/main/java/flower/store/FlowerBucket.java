9216c9e86d1a187558a89049be62c95fda10c5ee
Dimitro2021 <dmytro.hrebeniuk@ucu.edu.ua>
2022-10-20 20:21:15 +0300

    flower bucket

---
+++ src/main/java/flower/store/FlowerBucket.java
@@ -0,0 +1,16 @@
+package flower.store;
+
+import java.util.ArrayList;
+
+public class FlowerBucket {
+    private int allPrice = 0;
+    private ArrayList<FlowerPack> packList = new ArrayList<>();
+    public int getPrice() {
+        return allPrice;
+    }
+
+    public void add(FlowerPack flowerPack) {
+        packList.add(flowerPack);
+        allPrice += flowerPack.getPrice();
+    }
+}
--- src/main/java/flower/store/FlowerPack.java
+++ src/main/java/flower/store/FlowerPack.java
@@ -1,6 +1,13 @@
 package flower.store;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class FlowerPack {
     private Flower flower;
     private int quantity;
+
+    public double getPrice(){
+        return flower.getPrice() * quantity;
+    }
 }
