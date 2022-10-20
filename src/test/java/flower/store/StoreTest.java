45d624101b13e3cdfa8d9a4baa01e82f31515ce5
Dimitro2021 <dmytro.hrebeniuk@ucu.edu.ua>
2022-10-20 22:29:11 +0300

    final

--- src/test/java/flower/store/StoreTest.java
+++ src/test/java/flower/store/StoreTest.java
@@ -1,12 +1,16 @@
-//package flower.store;
-//
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.Assertions;
-//
-//public class StoreTest {
-//    public void testStore(){
-//        Store store = new Store();
-//        Tulip tulip = new Tulip();
-//        Chamomile chamomile = new Chamomile();
-//    }
-//}
+package flower.store;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class StoreTest {
+    @Test
+    public void testStore(){
+        Store store = new Store();
+        Rose rose = new Rose(1, FlowerColor.RED, 44);
+        Tulip tulip = new Tulip(1, FlowerColor.RED, 44);
+        store.addFlower(rose);
+        store.addFlower(tulip);
+        Assertions.assertEquals(rose, store.search(rose));
+    }
+}
