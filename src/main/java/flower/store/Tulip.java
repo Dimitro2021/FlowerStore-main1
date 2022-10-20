0256aaaf6098be72c4cc39399b87df8bfeac658c
Dimitro2021 <dmytro.hrebeniuk@ucu.edu.ua>
2022-10-20 23:03:44 +0300

    linter

--- src/main/java/flower/store/Tulip.java
+++ src/main/java/flower/store/Tulip.java
@@ -4,4 +4,5 @@ public class Tulip extends Flower {
     public Tulip(double sepalLength, FlowerColor color, double price) {
         super(sepalLength, color, price, FlowerType.TULIP);
     }
+    public Tulip(){}
 }
--- src/test/java/flower/store/StoreTest.java
+++ src/test/java/flower/store/StoreTest.java
@@ -7,10 +7,15 @@ public class StoreTest {
     @Test
     public void testStore() {
         Store store = new Store();
-        final int LENGTH = 1;
-        final int PRICE = 45;
-        Rose rose = new Rose(LENGTH, FlowerColor.RED, PRICE);
-        Tulip tulip = new Tulip(LENGTH, FlowerColor.RED, PRICE);
+//        final int LENGTH = 1;
+//        final int PRICE = 45;
+//        Rose rose = new Rose(LENGTH, FlowerColor.RED, PRICE);
+//        Tulip tulip = new Tulip(LENGTH, FlowerColor.RED, PRICE);
+        Rose rose = new Rose();
+        Tulip tulip = new Tulip();
+        rose.setColor(FlowerColor.RED);
+        tulip.setColor(FlowerColor.BLUE);
+
         store.addFlower(rose);
         store.addFlower(tulip);
         Assertions.assertEquals(rose, store.search(rose));
